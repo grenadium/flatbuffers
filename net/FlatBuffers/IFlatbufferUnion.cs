7af102360040ed2118bf5504b8b2270c02091149
Alexandre Pia <alex@middlevr.com>
2019-08-13 13:14:18 +0200

    [C#] Add IFlatbufferUnion<TEnum> interface and Union struct

---
+++ net/FlatBuffers/IFlatbufferUnion.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace FlatBuffers
+{
+    /// <summary>
+    /// This is the base for all Union types
+    /// </summary>
+    public interface IFlatbufferUnion<TEnum> where TEnum : struct
+    {
+        void __init(TEnum type, int _tableOffset, ByteBuffer _bb);
+
+        int Clone(FlatBufferBuilder builder);
+
+        TEnum Type { get; }
+
+        TTable Get<TTable>() where TTable : struct, IFlatbufferObject<TTable>;
+    }
+
+    /// <summary>
+    /// This is the base for all collections of Union types
+    /// </summary>
+    public interface IFlatbufferUnionCollection
+    {
+        void __init(int enumVectorOffset, int tableVectorOffset, ByteBuffer bb);
+        VectorOffset Clone(FlatBufferBuilder builder);
+    }
+}
---
+++ net/FlatBuffers/Union.cs
@@ -0,0 +1,45 @@
+﻿namespace FlatBuffers
+{
+    public struct Union<TEnum> where TEnum : struct
+    {
+        public TEnum type { get; private set; }
+        public int bb_pos { get; private set; }
+        public ByteBuffer bb { get; private set; }
+
+        // Re-init the internal state with an external buffer {@code ByteBuffer} and an offset within.
+        public Union(TEnum _t, int _i, ByteBuffer _bb)
+        {
+            type = _t;
+            bb = _bb;
+            bb_pos = _i;
+        }
+
+        public TTable __to<TTable>() where TTable : IFlatbufferObject
+        {
+            TTable t = default(TTable);
+            t.__init(bb_pos, bb);
+            return t;
+        }
+
+        public static VectorOffset __clone<TUnion, TUnionCollection>(FlatBufferBuilder builder, TUnionCollection collection)
+            where TUnion : struct, IFlatbufferUnion<TEnum>
+            where TUnionCollection : struct, IFlatbufferCollection<TUnion>
+        {
+            using (FlatBufferBuilder.CachedArray cache = builder.GetCachedArray(collection.Count))
+            {
+                int[] offsets = cache.Array;
+                for (int i = collection.Count - 1; i >= 0; i--)
+                {
+                    offsets[i] = collection[i].Clone(builder);
+                }
+
+                builder.StartVector(sizeof(int), collection.Count, sizeof(int));
+                for (int i = collection.Count - 1; i >= 0; --i)
+                {
+                    builder.AddOffset(offsets[i]);
+                }
+                return builder.EndVector();
+            }
+        }
+    }
+}
